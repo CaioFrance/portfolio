acc63fe9f37fadd3fa810898a3aa379c5a88c0f4
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2022-01-07 00:22:04 -0400

    feat: add component About

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import '../assets/css/About.css';
+
+const About = () => {
+  return (
+    <div className="about-container" id="about">
+      <div className="about-text">Sobre mim</div>
+    </div>
+  );
+};
+
+export default About;

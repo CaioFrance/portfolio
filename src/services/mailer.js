8cc1418ead34eb755fa77a1dd18e6f9dcdcc74fc
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2022-01-09 18:54:57 -0400

    wip

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mailer = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3333' ?? process.env.REACT_APP_API_MAILER,
 });
 
 export default mailer;

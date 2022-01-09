e5cbdab750f1497904479bf3f4ddd795d3c2d0b4
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2022-01-09 19:09:15 -0400

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
+import {ImLocation, ImWhatsapp} from 'react-icons/im'
+
 import '../assets/css/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <address className="footer-address">
-        Manaus - AM <br />
-        Rua D 11, 102 <br />
-        CEP 69076-750 <br />
+        <span style={{marginRight: '10px'}}>
+          <ImLocation />
+        </span>
+        Manaus - AM, Rua D 11, 102 - CEP 69076-750 <br />
+        <span style={{marginRight: '10px'}}>
+          <ImWhatsapp />
+        </span>
         (92) 99255-6313
       </address>
       <p className="footer-text">
